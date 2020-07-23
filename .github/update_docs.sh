de856cf73ca17eb33cd01f1aeca5cf5a3b0698e8
Tobiah <tobiah@warframe.gg>
2020-07-22 21:56:03 -0500

    chore: update docs processing & deps

--- .github/update_docs.sh
+++ .github/update_docs.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 setup_git() {
+  git checkout ${TRAVIS_BRANCH}
   git config --global user.email "travis@travis-ci.com"
   git config --global user.name "Travis CI"
-  git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-  git checkout master
+  git config --global push.default current
 }
 
 publish_docs() {
   git add docs/.
-  git commit --message "chore(automated): Docs Update ${TRAVIS_BUILD_NUMBER} [ci skip]"
-  git push origin master
+  git commit -m "chore(automated): Docs Update ${TRAVIS_BUILD_NUMBER} [ci skip]"
+  git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 }
 
-setup_git
-npm run build-docs
-publish_docs
+if [ $(git tag --points-at HEAD) ]; then
+  setup_git
+  echo Regenerating documentation:
+  npm run build-docs
+  publish_docs
+else
+  echo Skipping doc regeneration since no new tag was created.
+fi
