8c42cae978f38d1393be26a240530b3665fc50bc
Matej Voboril <tobiah@pm.me>
2025-10-19 20:12:36 -0500

    fix: set default worldstate to 5 minutes & make it configurable

--- resources/config.js
+++ resources/config.js
@@ -1,6 +1,9 @@
-export const WORLDSTATE_URL = process.env.WORLDSTATE_URL ?? 'https://api.warframe.com/cdn/worldState.php';
-export const KUVA_URL = process.env.KUVA_URL ?? 'https://10o.io/arbitrations.json';
-export const SENTIENT_URL = process.env.SENTIENT_URL ?? 'https://semlar.com/anomaly.json';
+export const worldstateUrl = process.env.WORLDSTATE_URL ?? 'https://api.warframe.com/cdn/worldState.php';
+export const kuvaUrl = process.env.KUVA_URL ?? 'https://10o.io/arbitrations.json';
+export const sentientUrl = process.env.SENTIENT_URL ?? 'https://semlar.com/anomaly.json';
+
+export const worldstateCron = process.env.WORLDSTATE_CRON ?? '25 */5 * * * *';
+export const externalCron = process.env.WS_EXTERNAL_CRON ?? '0 */10 * * * *';
 
 export const FEATURES = process.env.WS_EMITTER_FEATURES
   ? process.env.WS_EMITTER_FEATURES.split(',')
