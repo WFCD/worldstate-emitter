4fdf07c9f6d0d2a56701e21ec679feef541ef004
Matej Voboril <tobiah@pm.me>
2026-04-06 12:02:01 -0500

    fix(output): dual esm/cjs output

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,4 +3,23 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
   entry: ['index.ts'],
   dts: true,
+  outExtensions: (context) => {
+    let js = '.js';
+    let dts = '.d.ts';
+    switch (context.format) {
+      case 'cjs':
+        js = '.js';
+        break;
+      case 'es':
+        js = '.mjs';
+        dts = '.d.mts';
+        break;
+      case 'umd':
+      case 'iife':
+        js = '.js';
+        break;
+    }
+    return { js, dts };
+  },
+  format: ['esm', 'cjs'],
 });
